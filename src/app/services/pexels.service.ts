a1cb80dfbdfa9327aaf7204bdc1cc2305123f29a
LIZBETH GRISALES CASTRO <lizgricas@gmail.com>
2023-09-25 14:26:24 -0500

    apikey despliegue en vercel

--- src/app/services/pexels.service.ts
+++ src/app/services/pexels.service.ts
@@ -1,11 +1,12 @@
-import { stringify } from "querystring";
+//import { stringify } from "querystring";
 
-const API_KEY = process.env.NEXT_PUBLIC_PEXELS_API_KEY;
+//const API_KEY = process.env.NEXT_PUBLIC_PEXELS_API_KEY;
 
 export const searchPexelsApi = async (query: string) => {
   const response = await fetch(`https://api.pexels.com/v1/search?query=${query}`, {
     headers: {
-      Authorization: API_KEY,
+      'Content-Type': 'application/json',
+     Authorization: 'gafdR2iEX4u4A7AAfijiy5pHjqhr2kpc44G4BQqJmc40a9MyqXiSf2Ry'
     },
   });
 
